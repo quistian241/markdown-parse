23357a553299a9d475edae0d2ca4f94dc4afea87
quistian241 <c7gonzalez@ucsd.edu>
2022-01-20 11:40:54 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,7 +16,9 @@ public class MarkdownParse {
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
+            System.out.println(currentIndex);
             currentIndex = closeParen + 1;
+            
         }
         return toReturn;
     }

0bc3d48fece511263cd0fc5ca54f7b29f5d1b3e7
quistian241 <c7gonzalez@ucsd.edu>
2022-01-27 12:18:07 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,38 @@
+// javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
+// java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.*;
 
 public class MarkdownParseTest {
+    // System.out.println(MarkdownParse.getLinks(""));
+
     @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
+    public void testCorrectLink() {
+        assertEquals(List.of("https://something.com"), MarkdownParse.getLinks("[a link!](https://something.com)"));
+    }
+
+    @Test
+    public void testExtraSpace() {
+        assertEquals(List.of(), MarkdownParse.getLinks("[fake link]extra space here(not a real link)"));
+    }
+
+    @Test
+    public void testImages() {
+        assertEquals(List.of("google.com"), MarkdownParse.getLinks("[](google.com)"));
+    }
+
+    @Test
+    public void testPrints() {
+        System.out.println();
+        System.out.println("New test: " + MarkdownParse.getLinks("[First line link](first line link should be found)"));
+        System.out.println("New test: " + MarkdownParse.getLinks("[a] link!](https://something.com)"));
+        System.out.println("New test: " + MarkdownParse.getLinks("a text!"));
+        System.out.println("New test: " + MarkdownParse.getLinks("a ] ] ] ] text!"));
+        System.out.println("New test: " + MarkdownParse.getLinks(""));
+
+
+
+        
     }
 }

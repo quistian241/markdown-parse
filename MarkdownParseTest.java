96c4ff64050ab306aad4c3f43fbe0dd57e40444b
quistian241 <c7gonzalez@ucsd.edu>
2022-02-10 12:08:09 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.*;
 public class MarkdownParseTest {
     @Test
     public void addition() {
-        assertEquals(2, 3 + 1);
+        assertEquals(2, 1 + 1);
     }
 
     @Test

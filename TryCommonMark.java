73c348ea9d2d3946900de3bee4f68a44bf26eccd
quistian241 <c7gonzalez@ucsd.edu>
2022-03-10 11:51:21 -0800

    Update TryCommonMark.java

--- TryCommonMark.java
+++ TryCommonMark.java
@@ -3,32 +3,32 @@ import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
 
 class TryCommonMark {
-    // this part actually does the computation
-    Node node = parser.parse("Example\n=======\n\nSome more text");
-    WordCountVisitor visitor = new WordCountVisitor();
-    node.accept(visitor);
-    visitor.wordCount;  // 4
+    public static void main(String[] args) {
+        // Parser parser = Parser.builder().build();
+        // Node document = parser.parse("This is *Sparta*");
+        // HtmlRenderer renderer = HtmlRenderer.builder().build();
+        // System.out.println(renderer.render(document));  // "<p>This is <em>Sparta</em></p>\n"
+        
+        // this part actually does the computation
+        Node node = parser.parse("Example\n=======\n\nSome more text");
+        WordCountVisitor visitor = new WordCountVisitor();
+        node.accept(visitor);
+        visitor.wordCount;  // 4
 
     // this class can be defined anywhere in the file
     class WordCountVisitor extends AbstractVisitor {
         int wordCount = 0;
-
-        @Override
-        public void visit(Text text) {
-            // This is called for all Text nodes. Override other visit methods for other node types.
-
-            // Count words (this is just an example, don't actually do it this way for various reasons).
-            wordCount += text.getLiteral().split("\\W+").length;
-
-            // Descend into children (could be omitted in this case because Text nodes don't have children).
-            visitChildren(text);
-        }
-    }
-    public static void main(String[] args) {
-        Parser parser = Parser.builder().build();
-        Node document = parser.parse("This is *Sparta*");
-        HtmlRenderer renderer = HtmlRenderer.builder().build();
-        System.out.println(renderer.render(document));  // "<p>This is <em>Sparta</em></p>\n"
     }
 }
 
+@Override
+    public void visit(Text text) {
+        // This is called for all Text nodes. Override other visit methods for other node types.
+
+        // Count words (this is just an example, don't actually do it this way for various reasons).
+        wordCount += text.getLiteral().split("\\W+").length;
+
+        // Descend into children (could be omitted in this case because Text nodes don't have children).
+        visitChildren(text);
+    }
+}
